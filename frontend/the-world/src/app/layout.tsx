3329ed8c351661af98dadfeb17f0f1d7b0689e5b
Yixuan Shen <sheny22@rpi.edu>
2024-03-25 14:04:14 -0400

    Add basic connect to wallet

--- frontend/the-world/src/app/layout.tsx
+++ frontend/the-world/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Web3Provider } from "./connectkit_provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Web3Provider>{children}</Web3Provider>
+      </body>
     </html>
   );
 }
