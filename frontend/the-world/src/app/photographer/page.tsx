18c53f9f4ade3639e1887e63b8b5922b40bc76ae
Yixuan Shen <sheny22@rpi.edu>
2024-03-31 19:32:13 -0400

    Add home link and conditional rendering of client address

--- frontend/the-world/src/app/photographer/page.tsx
+++ frontend/the-world/src/app/photographer/page.tsx
@@ -2,15 +2,19 @@
 
 import { useModal } from "connectkit"
 import { useAccount, useDisconnect } from "wagmi"
+import Link from "next/link";
 import Image from "next/image";
 import styles from "../page.module.css";
 
 export default function Home() {
-  const { address } = useAccount();
+  const { address, isConnected } = useAccount();
   return (
-    <main className={styles.main}>
-      <h1>Welcome to the photographer page</h1>
-      <p> Photographer address is {address} </p>
+    <main >
+      <Link href="/" > Home </Link>
+      <div className={styles.main}>
+        <h1>Welcome to the photographer page</h1>
+        {isConnected && <p> Client address is {address} </p>}
+      </div>
     </main>
   );
 }
