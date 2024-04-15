f170f6b09f4a5ea7ace3faf4e4f4361c458de84c
Yixuan Shen <sheny22@rpi.edu>
2024-04-15 00:06:13 -0400

    Add file processing and IPFS upload logic to check_files.py

--- frontend/api/check_files.py
+++ frontend/api/check_files.py
@@ -5,7 +5,14 @@ sleep_time = 5
 
 
 def process_file(file_path) -> bool:
-    pass
+    file_passed = False
+    output = subprocess.run(
+        ['python', '../../AI/model.py', file_path], capture_output=True)
+    output_str = output.stdout.decode('utf-8').strip()
+    if output_str == 'Yes':
+        file_passed = True
+
+    return file_passed
 
 
 def upload_to_ipfs(file_path) -> str:
@@ -23,22 +30,21 @@ def main(path):
     while True:
         files = os.listdir(path)
         for file in files:
-            if file.endswith('.jpg') or file.endswith('.png'):
-                file_passed = process_file(os.path.join(path, file))
-                # file_passed = True
-                if file_passed:
-                    print(f"File {file} passed")
-                    os.rename(os.path.join(path, file),
-                              os.path.join('display', file))
-                    ipfs_code = upload_to_ipfs(os.path.join('display', file))
-                    print(f"IPFS link: ipfs.io/ipfs/{ipfs_code}")
-
-                else:
-                    print(f"File {file} failed")
-                    os.remove(os.path.join(path, file))
+            # if file.endswith('.jpg') or file.endswith('.png'):
+            file_passed = process_file(os.path.join(path, file))
+            # file_passed = True
+            if file_passed:
+                print(f"File {file} passed")
+                os.rename(os.path.join(path, file),
+                          os.path.join('display', file))
+                ipfs_code = upload_to_ipfs(os.path.join('display', file))
+                print(f"IPFS link: ipfs.io/ipfs/{ipfs_code}")
+                # TODO: Send the IPFS link to the backend
+            else:
+                print(f"File {file} failed")
+                os.remove(os.path.join(path, file))
 
         time.sleep(sleep_time)
-
     pass
 
 
